5b217b07671fa73b9fafa6ccb2eadf7b72307262
Amos Gheva <ghevaamos@gmail.com>
2019-06-30 14:15:31 -0400

    Adding multiplication exercises to the multiplication subject

--- src/mathex/multiplication.cpp
+++ src/mathex/multiplication.cpp
@@ -1,6 +1,8 @@
 #include "multiplication.h"
 #include "countby.h"
 #include "multable.h"
+#include "multiplyby.h"
+#include "digitbydigit.h"
 
 namespace mathex
 {
@@ -10,6 +12,15 @@ Multiplication::Multiplication() :
 {
   add_level(new CountBy);
   add_level(new MulTable);
+  add_level(new MultiplyBy(2, true));
+  add_level(new MultiplyBy(5, true));
+  add_level(new MultiplyBy(4, true));
+  add_level(new MultiplyBy(3, true));
+  add_level(new MultiplyBy(6, true, 5));
+  add_level(new MultiplyBy(7, true, 5));
+  add_level(new MultiplyBy(8, true, 5));
+  add_level(new MultiplyBy(9, true, 5));
+  add_level(new DigitByDigit());
 }
 
 Multiplication::~Multiplication()
