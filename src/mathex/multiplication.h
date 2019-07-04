f27edc6c8eb6b0e6cd009c3aa69159dc9fb23897
Amos Gheva <ghevaamos@gmail.com>
2019-07-04 13:29:18 -0400

    Exposing the multiplication levels for use not in the context of a topic

--- src/mathex/multiplication.h
+++ src/mathex/multiplication.h
@@ -17,6 +17,11 @@ private:
 
 } // namespace
 
+#include "mathex/plustomul.h"
+#include "mathex/countby.h"
+#include "mathex/multiplyby.h"
+#include "mathex/multable.h"
+
 #endif // MATHEX_MULTIPLICATION_H__
 
 /* vim: set cindent sw=2 expandtab : */
